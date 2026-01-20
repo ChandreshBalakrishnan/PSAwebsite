994278c0f252e359061c4325f92e7c4cb2b63e6a
Chandresh <64546911+ChandreshBalakrishnan@users.noreply.github.com>
2026-01-20 18:31:14 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,14 +1,11 @@
+const viewer = document.getElementById("viewer");
 const tabs = document.querySelectorAll(".tab");
-const viewers = document.querySelectorAll(".viewer");
 
 tabs.forEach(tab => {
   tab.addEventListener("click", () => {
-    // Remove active classes
     tabs.forEach(t => t.classList.remove("active"));
-    viewers.forEach(v => v.classList.remove("active"));
-
-    // Activate selected tab and viewer
     tab.classList.add("active");
-    document.getElementById(tab.dataset.tab).classList.add("active");
+
+    viewer.src = tab.dataset.model;
   });
 });
